a8a5556ed3bc0606acda5950fb628440ef4604b7
Jeisson Alexander Gavilan Murcia <alexamarok260@gmail.com>
2023-03-02 20:03:40 -0500

    Se avanza en el main

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,22 +1,69 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Inicio = () => {
   return (
-    <>
+    <Fragment>
       <header>Wendy</header>
 
       <main>
-        <section>
+        <section className='fondo_banner'>
           <figure>
-            <img src='' alt='' />
+            <h1>SABROSAP</h1>
           </figure>
         </section>
-        <section></section>
-        <section></section>
+        <section className='categorias'>
+          <article>
+            <figure>
+              <figcaption></figcaption>
+              <img src='./' alt='' />
+            </figure>
+          </article>
+
+          <article>
+            <ul>
+              <li>
+                <button> Alimento</button>
+              </li>
+
+              <li>
+                <button> Alimento</button>
+              </li>
+
+              <li>
+                <button> Alimento</button>
+              </li>
+            </ul>
+          </article>
+        </section>
+
+        <section className='recetas'>
+          <article>
+            <figure>
+              <img src='' />
+            </figure>
+
+            <article>
+              <h3>Food Name</h3>
+              <p>
+                El aceite de oliva es un aceite vegetal saludable y nutritivo que se extrae de las aceitunas. Es rico en
+                grasas monoinsaturadas y polifenoles, lo que lo convierte en un ingrediente versátil en la cocina y
+                beneficioso para la salud. Su sabor y aroma característicos lo hacen ideal para ensaladas, aderezos,
+                guisos y otras preparaciones culinarias.
+              </p>
+
+              <ul>
+                <li>ingrediente</li>
+                <li>ingrediente</li>
+                <li>ingrediente</li>
+              </ul>
+            </article>
+          </article>
+        </section>
+        <a h />
       </main>
 
       <footer>Adriana</footer>
-    </>
+    </Fragment>
   );
 };
 
