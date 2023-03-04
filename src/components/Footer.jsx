564ee9770def6a9c82b580d79e2d5532c063a133
ADRIANA_MORENO_GUTIERREZ <adrianamorenogutierrez@gmail.com>
2023-03-03 20:55:17 -0500

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import "../styles/footer.scss";
+
+export const Footer = () => {
+  return (
+    <footer className="container-fluid">
+      <div className="main-container">
+        <div className="row">
+          <div className="col-md-4 text-center align-middle">
+            <img
+              className="rounded-circle main-logo"
+              alt="171x180"
+              src="https://raw.githubusercontent.com/Alexander206/bit10team/wendyCarlos/src/assets/document/logoTecnoPower.png"
+            ></img>
+          </div>
+
+          <div className="col-md-2 text-center align-middle">
+            <img
+              className="rounded-circle circle"
+              alt="171x180"
+              src="https://media.licdn.com/dms/image/D5603AQGR7ZPTAQvh9w/profile-displayphoto-shrink_400_400/0/1665088793878?e=1683158400&v=beta&t=_vKmIevoFEh6Ai-Hx5cEXpqpqWxe-qq3OfX95_S43CQ"
+            ></img>
+            <div className="row">
+              <div className="col-md-12 text-center name">Jeisson Gavilán</div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <a
+                  href="https://www.linkedin.com/in/jeisson-alexander/"
+                  target="_blank"
+                >
+                  <img
+                    className="rounded-circle logo1"
+                    alt="Logo LinkedIng"
+                    src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-11640440452zi2ykndpw2.png"
+                  ></img>
+                </a>
+              </div>
+              <div className="col-md-6">
+                <a href="https://github.com/Alexander206" target="_blank">
+                  <img
+                    className="rounded-circle logo2"
+                    alt="Logo GitHub"
+                    src="https://git-scm.com/images/logos/downloads/Git-Icon-White.png"
+                  ></img>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2 text-center align-middle">
+            <img
+              className="rounded-circle circle"
+              alt="171x180"
+              src="https://lh3.googleusercontent.com/a/AGNmyxZgfEKpYKuQMPqK5Ub2QI4qlMmh5u8XzNLJtHtMnQ=s192-c-mo"
+            ></img>
+            <div className="row">
+              <div className="col-md-12 text-center name">Wendy Carlos</div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <a
+                  href="https://www.linkedin.com/in/wendy-alejandra-carlos-mu%C3%B1oz/"
+                  target="_blank"
+                >
+                  <img
+                    className="rounded-circle logo1"
+                    alt="Logo LinkedIng"
+                    src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-11640440452zi2ykndpw2.png"
+                  ></img>
+                </a>
+              </div>
+              <div className="col-md-6">
+                <a href="https://github.com/wenaleja" target="_blank">
+                  <img
+                    className="rounded-circle logo2"
+                    alt="Logo GitHub"
+                    src="https://git-scm.com/images/logos/downloads/Git-Icon-White.png"
+                  ></img>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-2 text-center align-middle">
+            <img
+              className="rounded-circle circle"
+              alt="171x180"
+              src="https://media.licdn.com/dms/image/D4E03AQGpfbkQ0eO1OA/profile-displayphoto-shrink_400_400/0/1675216052678?e=1683158400&v=beta&t=lpzIH-WmTE4nxlcO8TNvXpgypQPkj9be8w1IFHSKTjA"
+            ></img>
+            <div className="row">
+              <div className="col-md-12 text-center name">Adriana Moreno</div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 text-center">
+                <a
+                  href="https://www.linkedin.com/in/adrianamorenogutierrez/"
+                  target="_blank"
+                >
+                  <img
+                    className="rounded-circle logo1"
+                    alt="Logo LinkedIn"
+                    src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-11640440452zi2ykndpw2.png"
+                  ></img>
+                </a>
+              </div>
+              <div className="col-md-6 text-center">
+                <a
+                  href="https://github.com/soyadrianamorenogutierrez"
+                  target="_blank"
+                >
+                  <img
+                    className="rounded-circle logo2"
+                    alt="Logo GitHub"
+                    src="https://git-scm.com/images/logos/downloads/Git-Icon-White.png"
+                  ></img>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-2 text-center align-middle">
+            <img
+              className="rounded-circle circle"
+              alt="171x180"
+              src="https://media.licdn.com/dms/image/D4E03AQHr-po9BKVF4g/profile-displayphoto-shrink_800_800/0/1675461674838?e=1683158400&v=beta&t=FEOMfrFGe6jLowOPNCUYVKlsrOH4kWzPdVTLZOj92YA"
+            ></img>
+            <div className="row">
+              <div className="col-md-12 text-center name">Eleiza Caridad</div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 text-center">
+                <a
+                  href="https://www.linkedin.com/in/eleiza-caridad/"
+                  target="_blank"
+                >
+                  <img
+                    className="rounded-circle logo1"
+                    alt="Logo LinkedIn"
+                    src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-11640440452zi2ykndpw2.png"
+                  ></img>
+                </a>
+              </div>
+              <div className="col-md-6 text-center">
+                <a href="https://github.com/eleizac" target="_blank">
+                  <img
+                    className="rounded-circle logo2"
+                    alt="Logo GitHub"
+                    src="https://git-scm.com/images/logos/downloads/Git-Icon-White.png"
+                  ></img>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import { Footer } from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  return <div className='App'>En construción</div>;
+  return (
+    <div className="App">
+      <header style={{ height: "30vh" }}></header>
+      <div style={{ height: "60vh" }}>TEXTO</div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
