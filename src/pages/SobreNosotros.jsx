0fdd2afd2015ead58b233b189b073cd94b4a3cb0
ADRIANA_MORENO_GUTIERREZ <adrianamorenogutierrez@gmail.com>
2023-03-04 09:53:44 -0500

    Inicio page SobreNosotros

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export const SobreNosotros = () => {
-  return (
-    <h1>SobreNosotros</h1>
-  )
-}
+  return <h1>SobreNosotros</h1>;
+  <section className="d-flex flex-column">
+    <article>
+      <Figure.image
+      roundedCircle
+      with= {150}
+      height= {150}
+      src=""
+      />
+    </article>
+  </section>
+
+};
