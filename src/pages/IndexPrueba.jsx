eef06e2eac5b1a44c9416cbd627c6169042bde77
trascent <luchopodex@hotmail.com>
2023-03-07 23:32:08 -0500

    Creación de vista de prueba de lista de recipientes para manejar el CRUD y eliminar los datos de prueba (PRELIMINAR)

--- src/pages/IndexPrueba.jsx
+++ src/pages/IndexPrueba.jsx
@@ -1,60 +1,93 @@
-import React from 'react';
+import React from "react";
 import { Header } from "../components/Header.jsx";
 import { Footer } from "../components/Footer.jsx";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Table from "react-bootstrap/Table";
+import { useState } from "react"
 
 export const IndexPrueba = () => {
-    // Simulated Data
-    const recipes = [
-        {
-            id: 1,
-            nombre: "test1",
-            ingredientes: "test1",
-            descripcion: "test1",
-        },
-        {
-            id: 2,
-            nombre: "test2",
-            ingredientes: "test2",
-            descripcion: "test2",
-        },
-        {
-            id: 3,
-            nombre: "test3",
-            ingredientes: "test3",
-            descripcion: "test3",
-        },
-        {
-            id: 4,
-            nombre: "test4",
-            ingredientes: "test4",
-            descripcion: "test4",
-        },
-        {
-            id: 5,
-            nombre: "test5",
-            ingredientes: "test5",
-            descripcion: "test5",
-        },
-    ];
-    return (
-        <>
-            <Header />
-            <main>
-                <section>
-                    <>
-                        {animals.map((animal, index) => (
-                            <div key={index}>
-                                <div>Type: {animal.type}</div>
-                                <div>Age: {animal.age}</div>
-                            </div>
-                        ))}
-                    </>
-                </section>
-            </main>
-            <Footer />
-        </>
-    );
+  // Simulated Data RECIPES
+  const [recipes, setRecipes] = useState([
+    {
+      id: 1,
+      name: "test1",
+      description: "test1",
+      ingredients: "test1",
+    },
+    {
+      id: 2,
+      name: "test2",
+      description: "test2",
+      ingredients: "test2",
+    },
+    {
+      id: 3,
+      name: "test3",
+      description: "test3",
+      ingredients: "test3",
+    },
+    {
+      id: 4,
+      name: "test4",
+      description: "test4",
+      ingredients: "test4",
+    },
+    {
+      id: 5,
+      name: "test5",
+      description: "test5",
+      ingredients: "test5",
+    },
+  ]);
+
+  const handleDelete = (id) => {
+    // Eliminamos el elemento con el id dado
+    setRecipes(recipes.filter(function (item) {
+      return item.id !== id;
+    }));
+  };
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <>
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Recipe Name</th>
+                  <th>Ingredients</th>
+                  <th>Description</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {recipes.map((recipe) => (
+                  <tr key={recipe.id}>
+                    <td>{recipe.id}</td>
+                    <td>{recipe.name}</td>
+                    <td>{recipe.description}</td>
+                    <td>{recipe.ingredients}</td>
+                    <td>
+                      {" "}
+                      <button onClick={() => handleDelete(recipe.id)}>
+                        Delete
+                      </button>
+                      <button onClick={() => handleEdit(recipe.id)}>
+                        Edit
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 };
